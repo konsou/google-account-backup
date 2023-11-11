d04d9dc814f55a2b2c21870e3c02681a1354b713
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-11-11 14:53:05 +0200

    Include all contact data

--- contacts.py
+++ contacts.py
@@ -1,5 +1,38 @@
 import json
 
+# Checked 2023-11-11 from https://developers.google.com/people/api/rest/v1/people/get
+VALID_PERSON_FIELDS = (
+    "addresses",
+    "ageRanges",
+    "biographies",
+    "birthdays",
+    "calendarUrls",
+    "clientData",
+    "coverPhotos",
+    "emailAddresses",
+    "events",
+    "externalIds",
+    "genders",
+    "imClients",
+    "interests",
+    "locales",
+    "locations",
+    "memberships",
+    "metadata",
+    "miscKeywords",
+    "names",
+    "nicknames",
+    "occupations",
+    "organizations",
+    "phoneNumbers",
+    "photos",
+    "relations",
+    "sipAddresses",
+    "skills",
+    "urls",
+    "userDefined",
+)
+
 
 def save_contacts(contacts):
     with open('contacts_backup.json', 'w') as file:
@@ -10,5 +43,5 @@ def get_contacts(service):
     results = service.people().connections().list(
         resourceName='people/me',
         pageSize=1000,
-        personFields='names,emailAddresses').execute()
+        personFields=','.join(VALID_PERSON_FIELDS)).execute()
     return results.get('connections', [])
